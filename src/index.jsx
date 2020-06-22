0d71f3eedd06079d7d73d9f6fb2ce3dc6eeb50ac
dnikolaev <dnikolaev02@yandex.ru>
2020-06-22 09:57:09 +0300

    Сделал сборку

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import * as $ from 'jquery'
+import './styles/styles.css'
+import './styles/less.less'
+import './styles/scss.scss'
+
+
+// console.log('JSON:', json)
+// console.log('XML:', xml)
+// console.log('CSV:', csv)
